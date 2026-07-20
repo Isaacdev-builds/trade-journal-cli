3d2d09cd20d903f4d4873024acac3a127fe0116b
Isaacdev-builds <isaaclgarcia807@gmail.com>
2026-07-19 20:00:49 -0400

    Removed redundant autosave message

--- storage_system.py
+++ storage_system.py
@@ -4,10 +4,7 @@ import os
 def save_trades(trades):
     with open("trades.json", "w", encoding="utf-8") as file:
         json.dump(trades, file, indent=4)
-
-    print("Trades saved successfully.")
-
-
+        
 def load_trades():
     if not os.path.exists("trades.json"):
         return []
